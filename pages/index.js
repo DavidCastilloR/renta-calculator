5054fd76477ec0d95ce8133af40c31ea6e9dda5f
davidcastillor <davidccrr3107@gmail.com>
2022-01-16 18:43:13 -0600

    version 1

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,61 @@
+import { useState } from 'react';
 import styles from '../styles/Home.module.css';
-import { useForm, useWatch } from 'react-hook-form';
 
 
 const HomeContent = () => {
 
-  const limits = [
-    { inf: 800_000, sup: 1_000_000, percentage: 0.1 },
-    { inf: 1_000_000, sup: 2_000_000, percentage: 0.15 },
-    { inf: 2_000_000, sup: 3_000_000, percentage: 0.2 },
-    { inf: 3_000_000, sup: 4_000_000, percentage: 0.25 }
+  const [salary, setSalary] = useState(0);
+  const [categories, setCategories] = useState([]);
+
+  const terms = [
+    { inf: 863_000, sup: 1_267_000, percentage: 0.1, category: '10%' },
+    { inf: 1_267_000, sup: 2_223_000, percentage: 0.15, category: '15%' },
+    { inf: 2_223_000, sup: 4_445_000, percentage: 0.2, category: '20%' },
+    { inf: 4_445_000, sup: Infinity, percentage: 0.25, category: '25%' }
   ];
 
-  const { register, control, handleSubmit } = useForm();
+  const onChange = (event) => {
+    setSalary(event.target.value);
+  }
 
 
-  const salary = useWatch({
-    control,
-    name: "salary", // without supply name will watch the entire form, or ['firstName', 'lastName'] to watch both
-    defaultValue: "default" // default value before the render
-  });
+  const calculate = () => {
+    let arrCats = []
+    for (const term of terms) {
+      if (salary < term.sup && salary >= term.inf) {
+        arrCats.push({ ...term, amount: (salary - term.inf) * term.percentage });
+        setCategories(arrCats)
+        return;
+      }
+      arrCats.push({ ...term, amount: (term.sup - term.inf) * term.percentage });
+    }
+  }
 
 
 
-  console.log(salary)
+
+
+
 
   return (
     <div className={styles.container}>
 
-      <input type="number" {...register("salary", { valueAsNumber: true })} />
+      <input type="number" onChange={onChange} />
+      <button onClick={calculate}>Calcular</button>
 
       <table>
         <tr>
-          <th>dgdfg</th>
-          <th>dgdfg</th>
-          <th>dgdfg</th>
+          <th>Salario</th>
+          {categories.map(c => (<th>{c.category}</th>))}
+          <th>impuesto</th>
+          <th>neto</th>
         </tr>
 
         <tr>
-          <td>dgfg</td>
-
-          <td>dgfg</td>
-          <td>dgfg</td>
+          <td>{salary}</td>
+          {categories.map((c, i) => (<td style={{ paddingLeft: 12 }}>{c.amount}</td>))}
+          <td> {categories.reduce((z, n) => z + n.amount, 0)}</td>
+          <td> {salary - categories.reduce((z, n) => z + n.amount, 0)}</td>
         </tr>
       </table>
 
@@ -50,7 +65,7 @@ const HomeContent = () => {
 
 export default function Home() {
 
-  return (<form><HomeContent /></form>)
+  return (<HomeContent />)
 
 }
 
