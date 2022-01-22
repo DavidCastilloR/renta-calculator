403533aea285ddbc4c865456f2edfb4628198396
davecastil <davidccrr3107@gmail.com>
2022-01-22 11:27:13 -0600

    more responsiveness

--- pages/index.js
+++ pages/index.js
@@ -30,63 +30,66 @@ const HomeContent = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <div className="h-screen lg:p-80 flex flex-col justify-center items-center bg-background text-white">
+      <div className="h-screen flex flex-col justify-between items-center bg-background text-white">
 
-        <h1 className="text-black lg:text-3xl text-lg mt-10 lg:mt-20 lg:absolute lg:top-0 mb-10 font-bold">Calculadora de impuesto de renta</h1>
+        <h1 className="text-black lg:text-3xl text-lg mt-10 mb-10 font-bold">Calculadora de impuesto de renta</h1>
 
-        <div className="flex lg:flex-row flex-col justify-center space-x-5 space-y-2">
-          <div className="flex justify-center items-center"><label className="text-primary text-2xl">₡</label><input type="number" className="w-50 h-12 text-black p-2 rounded-lg drop-shadow-lg" onChange={onChange} /></div>
-          <button className="h-12 rounded-lg px-2 text-lg drop-shadow-lg bg-primary" onClick={handleSubmit} type='submit'>Calcular</button>
+        <div>
+
+          <div className="flex flex-row justify-center items-center space-x-5 mb-5">
+            <label className="text-primary text-2xl">₡</label>
+            <input type="number" className="w-50 h-12 px-2 text-black rounded-lg drop-shadow-lg" onChange={onChange} />
+            <button className="h-12 w-30 rounded-lg px-2 self-center text-lg drop-shadow-lg alg bg-primary" onClick={handleSubmit} type='submit'>Calcular</button>
+          </div>
+
+
+          {Boolean(result?.taxes) &&
+            <table className="my-2 mx-auto rounded-lg bg-primary overflow-hidden">
+              <thead className="m-10">
+                <tr>
+                  <th className="p-4 text-center font-bold underline hidden md:table-cell">Salario</th>
+                  {result?.applyingTerms.map(c => (<th key={c.category} className="p-4 text-center font-bold underline hidden md:table-cell">{c.category}</th>))}
+                  <th className="p-4 text-center font-bold underline hidden md:table-cell">Impuesto</th>
+                  <th className="p-4 text-center font-bold underline hidden md:table-cell">Neto</th>
+                </tr>
+
+              </thead>
+              <tbody className="m-10 flex-1 md:flex-none">
+                <tr className="flex flex-col flex-no wrap md:table-row">
+
+                  <td className="p-4 text-center">
+                    <div className="flex">
+                      <div className="md:hidden font-bold underline mr-10">Salario</div>
+                      {toColon(result?.grossSalary)}
+                    </div>
+                  </td>
+                  {result?.applyingTerms?.map((c, i) => (<td key={c.amount + i} className="p-4 text-center">
+                    <div className="flex">
+                      <div className="md:hidden font-bold underline mr-10">{c.category}</div>
+                      {c.amount ? toColon(c.amount) : 'N/A'}
+                    </div >
+                  </td>))}
+                  <td className="p-4 text-center">
+                    <div className="flex">
+                      <div className="md:hidden font-bold underline mr-10">Impuesto</div>
+                      {toColon(result?.taxes)}
+                    </div >
+                  </td>
+                  <td className="p-4 text-center">
+                    <div className="flex">
+                      <div className="md:hidden font-bold underline mr-10">Neto</div>
+                      {toColon(result?.netSalary)}
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          }
+          {result && Boolean(!result.taxes) && <p className="text-2xl text-primary my-10">No aplica</p>}
         </div>
 
 
-        {Boolean(result?.taxes) &&
-          <table className="my-10 rounded-lg bg-primary overflow-hidden">
-            <thead className="m-10">
-              <tr>
-                <th className="p-4 text-center font-bold underline hidden md:table-cell">Salario</th>
-                {result?.applyingTerms.map(c => (<th key={c.category} className="p-4 text-center font-bold underline hidden md:table-cell">{c.category}</th>))}
-                <th className="p-4 text-center font-bold underline hidden md:table-cell">Impuesto</th>
-                <th className="p-4 text-center font-bold underline hidden md:table-cell">Neto</th>
-              </tr>
-
-            </thead>
-            <tbody className="m-10 flex-1 md:flex-none">
-              <tr className="flex flex-col flex-no wrap md:table-row">
-
-                <td className="p-4 text-center">
-                  <div className="flex">
-                    <div className="md:hidden font-bold underline mr-10">Salario</div>
-                    {toColon(result?.grossSalary)}
-                  </div>
-                </td>
-                {result?.applyingTerms?.map((c, i) => (<td key={c.amount + i} className="p-4 text-center">
-                  <div className="flex">
-                    <div className="md:hidden font-bold underline mr-10">{c.category}</div>
-                    {c.amount ? toColon(c.amount) : 'N/A'}
-                  </div >
-                </td>))}
-                <td className="p-4 text-center">
-                  <div className="flex">
-                    <div className="md:hidden font-bold underline mr-10">Impuesto</div>
-                    {toColon(result?.taxes)}
-                  </div >
-                </td>
-                <td className="p-4 text-center">
-                  <div className="flex">
-                    <div className="md:hidden font-bold underline mr-10">Neto</div>
-                    {toColon(result?.netSalary)}
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        }
-        {result && Boolean(!result.taxes) && <p className="text-2xl text-primary my-10">No aplica</p>}
-
-
-
-        <p className="absolute bottom-0 text-black">v1.1. Creado por:  <a className="text-primary" href="mailto:me@david-castillo.com">David C.</a></p>
+        <p className="text-black">v1.1 - {(new Date()).getFullYear()}</p>
       </div>
     </form>
   )
