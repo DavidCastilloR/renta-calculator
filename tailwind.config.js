23ac6ceea9ac0f9919ec15e3e054913058be5892
davecastil <davidccrr3107@gmail.com>
2026-01-16 12:53:09 -0600

    update 2026

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     extend: {
       colors: {
         background: "#7fb069",
-        primary: "#ece4b7",
+        primary: "#d36135",
         black: "#FFFFF0",
       },
       screens: {
